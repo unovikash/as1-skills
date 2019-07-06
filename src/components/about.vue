12ae8935dbbe584c77bfe566da299f33936d4fbe
unovikash <vikashkumar@unomaha.edu>
2019-07-06 16:18:45 -0500

    Testing routing with params

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,6 +1,18 @@
 <template>
 	<div class="about">
 		<h1>About</h1>
+		<p>Hello {{their_name}}</p>
 		<p>The skills application was developed to learn more about the Vue framework. I could have added some random things as well, but you know what, at this point of time, I am just a tired soul, trying to get done with it. So, yes, hurray, I am almost done.</p>
 	</div>
 </template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
