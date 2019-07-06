dea1b85bb0642cc3631570646ac827c2099a322b
unovikash <vikashkumar@unomaha.edu>
2019-07-06 16:27:39 -0500

    Added skill remove option

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -17,6 +17,7 @@
         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
           <li v-for="(data, index) in skills" :key='index'>
             {{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
           </li>
         </transition-group>
       </ul>
@@ -49,6 +50,9 @@ export default {
           console.log('Invalid entry');
         }
       })
+    },
+    remove(id){
+      this.skills.splice(id, 1);
     }
   }
 }
@@ -58,6 +62,7 @@ export default {
 <style scoped>
 
   @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
     background: #fff;
@@ -123,6 +128,11 @@ export default {
     100% {
       transform: scale(1);
     }
+  }
+
+  i {
+    float: right;
+    cursor: pointer;
   }
 
 </style>
