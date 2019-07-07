2b281f9b7c10ee9f6ed7aeaf216ed8def02ad723
unovikash <vikashkumar@unomaha.edu>
2019-07-07 11:32:22 -0500

    Changed About to display name

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="about">
 		<h1>About</h1>
-		<p>The skills application was developed to learn more about the Vue framework. I could have added some random things as well, but you know what, at this point of time, I am just a tired soul, trying to get done with it. So, yes, hurray, I am almost done.</p>
+		<p>This is Kumar Vikash's Skills list. The skills application was developed to learn more about the Vue framework. I could have added some random things as well, but you know what, at this point of time, I am just a tired soul, trying to get done with it. So, yes, hurray, I am almost done.</p>
 	</div>
 </template>
